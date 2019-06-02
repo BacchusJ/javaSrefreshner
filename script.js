e55e8d130be5279ed0997a800b51a3eb1d31a6c7
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-02 11:03:50 -0500

    practice

---
+++ script.js
@@ -0,0 +1,47 @@
+// var firstName = 'Cristobal';
+// console.log(firstName);
+
+// var lastName = ('Aguirre');
+// var age = 2.5;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'bad ass kid';
+// console.log(job);
+
+
+// alert(firstName + ' ' + age + ' ' + 'is' + ' ' + job);
+
+// var lastName = prompt('What is his lat name');
+// console.log(firstName + ' ' + lastName);
+
+
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// math operators
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//logical operator
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// type of operator
+
+console.log(typeof johnOlder);
