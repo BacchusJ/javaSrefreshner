3e9feeb883bd3026cca4153dffe9c29cb7e76575
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-04 15:16:53 -0500

    objects and methods challange

--- script.js
+++ script.js
@@ -256,8 +256,7 @@ yearsUntilRetirement (1990, "John");
 yearsUntilRetirement (1930, "Mike");
 
 *******************************************
-*/
-
+*//*
 
 //arrays
 
@@ -333,11 +332,61 @@ jane.birthYear = 1969;
 jane['lastNam'] = 'Smith';
 console.log(jane);
 
+*/
+/*
+**********************************
 var john = {
     firstName: 'John',
     lastName: 'Smith',
-    birthYear: 1990,
+    birthYear: 1993,
     family: ['Jane', 'Mark', 'Bob', 'Emily'],
     job: 'teacher',
-    married: false
+    married: false,
+
+// add a function to our object that will calculate the age of John
+    calcAge: function() {
+        //the word "this" is refering to john (our currect object)
+        this.age = 2019 - this.birthYear;
+    }
 };
+
+
+
+john.calcAge();
+console.log(john);
+
+*/
+
+var mark = {
+    fullName: 'Mark Miller',
+    height: 1.89,
+    weight: 97.1,
+    calcMass: function() {
+       this.bmi = this.weight / (this.height * this.height);
+       return this.bmi;
+    }
+
+}
+mark.calcMass();
+console.log(mark);
+
+
+var john = {
+    fullName: 'John Smith',
+    height: 177,
+    weight: 90.71,
+    calcMass: function() {
+        this.bmi=this.weight / (this.height * this.height);
+        return this.bmi;
+    }
+}
+john.calcMass();
+console.log(john);
+
+
+if (mark.bmi > john.bmi) {
+    console.log (mark.fullName + ' \'s BMI of ' + mark.bmi  + ' is higher than John\'s there for, he wins! ');
+
+} else {
+    console.log(john.fullName + ' \'s BMI of ' + john.bmi + 'is higher than John\'s there for, he wins! ');
+}
