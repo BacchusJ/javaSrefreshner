4ad740a56da0ff63fc024c022d8f33555b3dd4e4
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-05 15:22:13 -0500

    finished lastest challange

--- script.js
+++ script.js
@@ -409,7 +409,7 @@ while(i < john.length){
     console.log(john[i]);
     i++;
 }
-*/
+
 var john = ['John', 'Smith', 1990, 'designer', false];
 for (var i = 0; i < john.length; i++) {
     if (typeof john[i] !== "string") continue
@@ -425,4 +425,78 @@ for (var i = 0; i < john.length; i++) {
 
 for (var i = john.length -1; i >= 0; i --) {
     console.log(john[i]);
+}
+
+*/
+
+var john = {
+    fullName: 'John Smith',
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            //determine percentage
+            var percentage;
+            var bill = this.bills[i];
+            if (bill < 50){
+                percentage = .2;
+            }else if (bill >= 50 && bill < 200) {
+                percentage = .15
+            } else {
+                percentage = .1
+            }
+            //add results to corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 475, 110, 45,],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            //determine percentage
+            var percentage;
+            var bill = this.bills[i];
+            if (bill < 100){
+                percentage = .2;
+            }else if (bill >= 100 && bill < 300) {
+                percentage = .1;
+            } else {
+                percentage = .25;
+            }
+            //add results to corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+function calcAverage(tips) {
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+// do all the calculations 
+
+john.calcTips()
+mark.calcTips()
+
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+    console.log(john.fullname + '\'s family pays higher tipes with an average of $' + john.average) 
+}else if (mark.average > john.average) {
+    console.log(mark.fullName + '\'s family pays higher tipes with an average of $' + mark.average)
 }
